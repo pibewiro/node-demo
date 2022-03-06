f4f45e5674f29631b8a0a0f80f536f6c763b449b
pibewiro <pibewiro@yahoo.com>
2022-03-06 15:30:07 -0300

    aws-node

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000, () => console.log("Server started at port 3000"));
+app.listen(8000, () => console.log("Server started at port 8000"));
